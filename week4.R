b75fd6d5c9103d9fc2b885109ec7eb6569ee14ad
imethun <imethuns7@gmail.com>
2020-09-30 02:22:45 +0600

    with rank hospital function

--- week4.R
+++ week4.R
@@ -77,3 +77,64 @@ best("TX", "heart attack")
 best("TX", "heart failure")
 best("TX", "pneumonia")
 best("MD", "heart attack")
+
+
+
+### --------------------------------------------------------------------------------------------
+
+##--------------------------------------------------------------------------------------------
+
+##--------------------------------------------------------------------------------------------
+
+## creating function of rankhospital 
+
+rankhospital<-function(state,outcome,num=1){
+  #reading the data set 
+  data<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  #reduce data set 
+  r_data<-data.frame(cbind(data[,2],
+                           data[,7],
+                           data[,11],
+                           data[,17],
+                           data[,23]),
+                     stringsAsFactors=FALSE)
+  # change column name
+  colnames(r_data)<-c("hospitals","states","heart attack","heart failure", "pneumonia")
+  #find valid state
+  if (! state %in% r_data[,"states"]){
+    stop("invalid state")
+  }
+  #find valid outcome 
+  if(! outcome %in% c("heart attack","heart failure", "pneumonia")){
+    stop("invalid outcome")
+  }
+  
+  # selecte cases that maches with desired state 
+  ss<-r_data[,"states"]==state
+  s_data<-r_data[ss,]
+  # make outcome variable numeric 
+  s_data[,outcome]<-as.numeric(s_data[,outcome])
+  #delete missing values
+  s_data<-s_data[!is.na(s_data[,outcome]),]
+  #order outcome variable 
+  s_data<-s_data[order(s_data[,outcome]),]
+  s_data$rank<-1:nrow(s_data)
+  
+  ##select correct number of row 
+  if(is.numeric(num)){
+    vv<-1:num
+  } else {
+    if(num=="best"){
+      vv<-1
+    }
+    if(num=="worst"){
+      vv<-nrow(s_data)
+    }
+  }
+  s_data<-s_data[vv,]
+  ## select correct variables
+  ttt<-which(colnames(s_data)==outcome)
+  paste(hospitas=s_data[,1],Rate=s_data[,ttt],Rank=s_data[,6])
+  
+}
+
